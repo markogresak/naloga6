f9dd8be671c8356828d5f4c9a8ed54f2bda9e7d6
Marko Grešak <marko@gresak.io>
2015-06-09 01:07:13 +0200

    remove , from .tab files

--- movielens-data/tab-to-csv.sh
+++ movielens-data/tab-to-csv.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+LC_ALL=C
 for file in 'cast' 'data' 'item' 'user' 'genre'; do
-  awk 'NR != 2' "$file.tab" | awk 'NR != 2' | LC_ALL=C tr '\t' ',' > "$file.csv"
+  awk 'NR != 2' "$file.tab" | awk 'NR != 2' | sed 's/,//g' | tr '\t' ',' > "$file.csv"
 done
