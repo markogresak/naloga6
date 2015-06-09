4a3a44b355e9d00d0b1bee78952b39cf8004055b
Marko Grešak <marko@gresak.io>
2015-06-09 11:46:32 +0200

    add table genres

--- db/schema.rb
+++ db/schema.rb
@@ -16,6 +16,12 @@ ActiveRecord::Schema.define(version: 20150609082804) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "genres", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.integer  "unknown"
     t.integer  "action"
