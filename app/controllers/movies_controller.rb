f7b8fa7ce0ddd863798bf420e6e185c74f0531a4
Marko Grešak <marko@gresak.io>
2015-06-09 20:12:34 +0200

    add algorithm to calculate k-nearest neighbor

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,41 @@ class MoviesController < ApplicationController
     # magic
     # ...
 
+    ratedMovies = current_user.movies
+    unratedMovies = Movie.all - ratedMovies
+
+    knn = []
+
+    puts "total length: #{ratedMovies.size * unratedMovies.size}"
+
+    ratedMovies.each do |ratedMovie|
+      ratedMovieUsers = ratedMovie.users
+      ratedMovieRatings = ratedMovie.ratings
+      unratedMovies.each do |unratedMovie|
+        unratedMovieUsers = unratedMovie.users
+        unratedMovieRatings = unratedMovie.ratings
+        usersRatedBoth = unratedMovieUsers & ratedMovieUsers
+        sumBoth = 0
+        sumRated = 0
+        sumUnrated = 0
+        usersRatedBoth.each do |userBoth|
+          bothRatings = userBoth.ratings
+          avgRating = bothRatings.average(:rating)
+          ratedRating = (bothRatings & ratedMovieRatings).first.rating
+          unratedRating = (bothRatings & unratedMovieRatings).first.rating
+          # or: u.ratings.where(:movie_id => m.ratings)
+          ux = (ratedRating - avgRating)
+          uy = (unratedRating - avgRating)
+          sumBoth += ux * uy
+          sumRated += ux * ux
+          sumUnrated += uy * uy
+        end
+        knn.push({movie_id: unratedMovie.id, similarity: sumBoth / Math.sqrt((sumRated * sumBoth).abs) })
+      end
+    end
+
+    puts knn
+
     @movies = Movie.limit(5)
     @ratings = Rating.group(:movie_id).average(:rating)
 
